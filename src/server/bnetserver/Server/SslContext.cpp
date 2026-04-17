145d670c097442556f73dc733122dfb7adcc0c45
slash-design <elite_slash@hotmail.com>
2026-04-17 22:24:02 +0200

    feat(bnetserver): make TLS certificate paths configurable
    Add `PublicKeyFile` and `PrivateKeyFile` options to
    `bnetserver.conf` and load them from `SslContext` via
    `ConfigMgr` instead of using hardcoded certificate paths.
    
    This allows using custom certificate and key locations
    without modifying the source code.
    
    Ref.: https://github.com/TrinityCore/TrinityCore/commit/7c3df3f5d3abfd7b270b4816ce714bdfe8c2a3b7

--- src/server/bnetserver/Server/SslContext.cpp
+++ src/server/bnetserver/Server/SslContext.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008-2016 TrinityCore <http://www.trinitycore.org/>
+ * This file is part of the DestinyCore Project. See AUTHORS file for Copyright information
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -17,6 +17,7 @@
 
 #include "SslContext.h"
 #include "Log.h"
+#include "Config.h"
 
 bool Battlenet::SslContext::Initialize()
 {
@@ -29,9 +30,12 @@ bool Battlenet::SslContext::Initialize()
         return false; \
     } } while (0)
 
+    std::string publicKeyFile = sConfigMgr->GetStringDefault("PublicKeyFile", "./bnetserver.cert.pem");
+    std::string privateKeyFile = sConfigMgr->GetStringDefault("PrivateKeyFile", "./bnetserver.key.pem");
+
     LOAD_CHECK(instance().set_options(boost::asio::ssl::context::no_sslv3, err));
-    LOAD_CHECK(instance().use_certificate_chain_file("bnetserver.cert.pem", err));
-    LOAD_CHECK(instance().use_private_key_file("bnetserver.key.pem", boost::asio::ssl::context::pem, err));
+    LOAD_CHECK(instance().use_certificate_chain_file(publicKeyFile, err));
+    LOAD_CHECK(instance().use_private_key_file(privateKeyFile, boost::asio::ssl::context::pem, err));
 
 #undef LOAD_CHECK
 
